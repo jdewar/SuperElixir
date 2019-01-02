b44fda625bbf5b93dcac5e9d02497e0126548b9e
Jeremy <jeremy.dewar@gmail.com>
2019-01-02 12:59:10 -0500

    update elixir_sense

--- elixir_sense/lib/elixir_sense/providers/definition.ex
+++ elixir_sense/lib/elixir_sense/providers/definition.ex
@@ -9,26 +9,40 @@ defmodule ElixirSense.Providers.Definition do
   alias ElixirSense.Core.Metadata
   alias ElixirSense.Core.Parser
   alias ElixirSense.Core.Introspection
+  alias ElixirSense.Core.State.VarInfo
 
-  @type file :: String.t
-  @type line :: pos_integer
-  @type location :: {file, line | nil}
+  defmodule Location do
+    @type t :: %Location{
+      found: boolean,
+      type: :module | :function | :variable,
+      file: String.t | nil,
+      line: pos_integer | nil,
+      column: pos_integer | nil
+    }
+    defstruct [:found, :type, :file, :line, :column]
+  end
 
   @doc """
-  Finds out where a module, function or macro was defined.
+  Finds out where a module, function, macro or variable was defined.
   """
-  @spec find(String.t, [module], [{module, module}], module) :: location
-  def find(subject, imports, aliases, module) do
-    subject
-    |> Introspection.split_mod_fun_call
-    |> Introspection.actual_mod_fun(imports, aliases, module)
-    |> find_source()
+  @spec find(String.t, [module], [{module, module}], module, [%VarInfo{}]) :: %Location{}
+  def find(subject, imports, aliases, module, vars) do
+    var_info = vars |> Enum.find(fn %VarInfo{name: name} -> to_string(name) == subject end)
+    case var_info do
+      %VarInfo{positions: [{line, column}|_]} ->
+        %Location{found: true, type: :variable, file: nil, line: line, column: column}
+      _ ->
+        subject
+        |> Introspection.split_mod_fun_call
+        |> Introspection.actual_mod_fun(imports, aliases, module)
+        |> find_source()
+    end
   end
 
   defp find_source({mod, fun}) do
     mod
     |> find_mod_file()
-    |> find_fun_line(fun)
+    |> find_fun_position(fun)
   end
 
   defp find_mod_file(module) do
@@ -49,21 +63,32 @@ defmodule ElixirSense.Providers.Definition do
     {module, file}
   end
 
-  defp find_fun_line({_, file}, _fun) when file in ["non_existing", nil, ""] do
-    {"non_existing", nil}
+  defp find_fun_position({_, file}, _fun) when file in ["non_existing", nil, ""] do
+    %Location{found: false}
   end
 
-  defp find_fun_line({mod, file}, fun) do
-    line = if String.ends_with?(file, ".erl") do
-      find_fun_line_in_erl_file(file, fun)
+  defp find_fun_position({mod, file}, fun) do
+
+    type = case fun do
+      nil -> :module
+      _ -> :function
+    end
+
+    position = if String.ends_with?(file, ".erl") do
+      find_fun_position_in_erl_file(file, fun)
     else
       file_metadata = Parser.parse_file(file, false, false, nil)
-      Metadata.get_function_line(file_metadata, mod, fun)
+      Metadata.get_function_position(file_metadata, mod, fun)
     end
-    {file, line}
+
+    case position do
+      {line, column} -> %Location{found: true, type: type, file: file, line: line, column: column}
+      _ -> %Location{found: true, type: type, file: file, line: 1, column: 1}
+    end
+
   end
 
-  defp find_fun_line_in_erl_file(file, fun) do
+  defp find_fun_position_in_erl_file(file, fun) do
     fun_name = Atom.to_string(fun)
     index =
       file
@@ -71,7 +96,7 @@ defmodule ElixirSense.Providers.Definition do
       |> String.split(["\n", "\r\n"])
       |> Enum.find_index(&String.match?(&1, Regex.recompile!(~r/^#{fun_name}\b/)))
 
-    (index || 0) + 1
+    {(index || 0) + 1, 1}
   end
 
 end
--- elixir_sense/test/elixir_sense/core/introspection_test.exs
+++ elixir_sense/test/elixir_sense/core/introspection_test.exs
@@ -21,13 +21,6 @@ defmodule ElixirSense.Core.IntrospectionTest do
   test "get_callbacks_with_docs for Elixir behaviours with no docs defined" do
     assert get_callbacks_with_docs(Exception) == [
       %{
-        arity: 2,
-        name: :blame,
-        callback: "@callback blame(t, stacktrace) :: {t, stacktrace}\n",
-        doc: "Called from `Exception.blame/3` to augment the exception struct.\n\nCan be used to collect additional information about the exception\nor do some additional expensive computation.\n",
-        signature: "blame(t, stacktrace)"
-      }, 
-      %{
         arity: 1,
         name: :exception,
         doc: nil,
@@ -40,12 +33,19 @@ defmodule ElixirSense.Core.IntrospectionTest do
         callback: "@callback message(t) :: String.t\n",
         doc: nil,
         signature: "message(t)"
-      }
+      },
+      %{
+        arity: 2,
+        name: :blame,
+        callback: "@callback blame(t, stacktrace) :: {t, stacktrace}\n",
+        doc: "Called from `Exception.blame/3` to augment the exception struct.\n\nCan be used to collect additional information about the exception\nor do some additional expensive computation.\n",
+        signature: "blame(t, stacktrace)"
+      },
     ]
   end
 
   test "get_callbacks_with_docs for Elixir behaviours with docs defined" do
-    info = get_callbacks_with_docs(GenServer) |> Enum.at(0)
+    info = get_callbacks_with_docs(GenServer) |> Enum.find(fn info -> info.name == :code_change end)
 
     assert info.name      == :code_change
     assert info.arity     == 3
@@ -100,9 +100,9 @@ defmodule ElixirSense.Core.IntrospectionTest do
     returns = get_returns_from_callback(:gen_server, :handle_call, 3)
     assert returns == [
       %{description: "{:reply, reply, new_state}", snippet: "{:reply, \"${1:reply}$\", \"${2:new_state}$\"}", spec: "{:reply, reply, new_state} when reply: term, new_state: term, reason: term"},
-      %{description: "{:reply, reply, new_state, timeout | :hibernate}", snippet: "{:reply, \"${1:reply}$\", \"${2:new_state}$\", \"${3:timeout | :hibernate}$\"}", spec: "{:reply, reply, new_state, timeout | :hibernate} when reply: term, new_state: term, reason: term"},
+      %{description: "{:reply, reply, new_state, timeout | :hibernate | {:continue, term}}", snippet: "{:reply, \"${1:reply}$\", \"${2:new_state}$\", \"${3:timeout | :hibernate | {:continue, term}}$\"}", spec: "{:reply, reply, new_state, timeout | :hibernate | {:continue, term}} when reply: term, new_state: term, reason: term"},
       %{description: "{:noreply, new_state}", snippet: "{:noreply, \"${1:new_state}$\"}", spec: "{:noreply, new_state} when reply: term, new_state: term, reason: term"},
-      %{description: "{:noreply, new_state, timeout | :hibernate}", snippet: "{:noreply, \"${1:new_state}$\", \"${2:timeout | :hibernate}$\"}", spec: "{:noreply, new_state, timeout | :hibernate} when reply: term, new_state: term, reason: term"},
+      %{description: "{:noreply, new_state, timeout | :hibernate, {:continue, term}}", snippet: "{:noreply, \"${1:new_state}$\", \"${2:timeout | :hibernate}$\", {:continue, term()}}", spec: "{:noreply, new_state, timeout | :hibernate, {:continue, term}} when reply: term, new_state: term, reason: term"},
       %{description: "{:stop, reason, reply, new_state}", snippet: "{:stop, \"${1:reason}$\", \"${2:reply}$\", \"${3:new_state}$\"}", spec: "{:stop, reason, reply, new_state} when reply: term, new_state: term, reason: term"},
       %{description: "{:stop, reason, new_state}", snippet: "{:stop, \"${1:reason}$\", \"${2:new_state}$\"}", spec: "{:stop, reason, new_state} when reply: term, new_state: term, reason: term"}
     ]
@@ -117,11 +117,21 @@ defmodule ElixirSense.Core.IntrospectionTest do
     ]
   end
 
+  test "get_types ignores privates types (:opaque and :typep)" do
+    types = get_types(ModuleWithPrivateTypes)
+    assert types == [type: {:type_t, {:type, 4, :atom, []}, []}]
+  end
+
+  test "get_func_docs_md works for kernel special forms" do
+    docs = get_func_docs_md(Kernel.SpecialForms, :__MODULE__)
+    assert docs == "> Kernel.SpecialForms.__MODULE__()\n\nReturns the current module name as an atom or `nil` otherwise.\n\nAlthough the module can be accessed in the `__ENV__/0`, this macro\nis a convenient shortcut.\n"
+  end
+
   defp get_type_ast(module, type) do
     {_kind, type} =
-      Kernel.Typespec.beam_types(module)
+      get_types(module)
       |> Enum.find(fn {_, {name, _, _}} -> name == type end)
-    Kernel.Typespec.type_to_ast(type)
+    type_to_quoted(type)
   end
 
 end
